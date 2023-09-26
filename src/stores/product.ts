3c08954bd5282dd985c28056ae97bf6a97876f04
Sam Macharia <smm_mwangii@live.com>
2023-09-26 02:45:57 +0100

    loading and error state

--- src/stores/product.ts
+++ src/stores/product.ts
@@ -7,25 +7,38 @@ export const useProductStore = defineStore('product', () => {
   const cartItems = ref<ProductWithQuantity[] | []>([])
   const product = ref<Product | null>(null)
 
+  const isLoading = ref(false)
+  const isError = ref(false)
+
   const getCartTotal = computed(() => {
     return cartItems.value.reduce((acc: number, item: ProductWithQuantity) => acc + item.price * item.quantity, 0)
   })
 
   const getAllProducts = async () => {
+    isLoading.value = true
+    isError.value = false
     try {
       const data = await getProducts()
       const response = await data.json()
       products.value = response.products
+      isLoading.value = false
     } catch (error) {
+      isLoading.value = false
+      isError.value = true
       console.error(error)
     }
   }
 
   const getProductItem = async (id: number) => {
+    isLoading.value = true
+    isError.value = false
     try {
       const data = await getProduct(id)
       product.value = await data.json()
+      isLoading.value = false
     } catch (error) {
+      isLoading.value = false
+      isError.value = true
       console.error(error)
     }
   }
